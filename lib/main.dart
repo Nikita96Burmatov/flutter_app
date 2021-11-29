7cc78dcab572e05a084bf5d74500cd271fb33aea
Nikita Burmatov <75168059+nikita96burmatov@users.noreply.github.com>
2021-11-29 23:14:05 +0300

    DrawerHeader

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
@@ -26,7 +27,7 @@ class MyStatefulWidget extends StatefulWidget{
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Index 0: Home',
@@ -41,44 +42,76 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       style: optionStyle,
     )
   ];
+
   void _onItemTapped(int index) {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text('NORMOCARD'),
         backgroundColor: Colors.indigo,
-    ),
-    body:  Center(
-         child: _widgetOptions.elementAt(_selectedIndex),
-    ),
-    bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-      icon:Icon(Icons.home),
-      label: 'Home',
-      backgroundColor: Colors.red,
       ),
-      BottomNavigationBarItem(
-      icon:Icon(Icons.business),
-      label: 'Business',
-      backgroundColor:  Colors.green,
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      BottomNavigationBarItem(
-      icon:Icon(Icons.school),
-      label: 'School',
-      backgroundColor: Colors.deepPurple,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Mrssages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
       ),
-    ],
-    currentIndex: _selectedIndex,
-    selectedItemColor: Colors.amber[800],
-    onTap: _onItemTapped,
-    ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.deepPurple,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
